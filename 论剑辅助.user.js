074be1e2cb5e9acb63875d35209015557c6ec4f6
Ellwizard <48432813+Ellwizard@users.noreply.github.com>
2019-07-05 15:58:43 +0800

    Update 论剑辅助.user.js

--- 论剑辅助.user.js
+++ 论剑辅助.user.js
@@ -4,9 +4,10 @@
 	// @version      3.1.0
 	// @description  脚本有风险 使用需谨慎
 	// @author       坏熊无双和毛毛
-	// @match        http://sword-direct1.yytou.cn:8081/*
-    // @match        http://sword-direct95.yytou.cn:8081/*
-	// @grant        none
+	// @match        http://*.yytou.cn/*
+        // @exclude      http://res.yytou.cn/*
+        // @exclude      http://sword.mud.yytou.
+       // @grant        none
 	// ==/UserScript==
 	if(window.top!=window){
 		window.top.location.href=window.location.href;
